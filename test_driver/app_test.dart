bdb9a3846f88e2ef95e556f8a2c4ce17b8e766da
Yoni Gutenmacher <yonigg98@gmail.com>
2019-07-31 09:50:37 -0400

    Driver tests updated

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -16,6 +16,16 @@ void main() {
     final deleteAccountFinder = find.byValueKey('delete account button');
     final signOutFinder = find.byValueKey('sign out button');
     final logInFinder = find.byValueKey('log-in');
+    final signUpPageButtonFinder = find.byValueKey('sign-up page button');
+    final signUpButtonFinder = find.byValueKey('sign-up button');
+    final firstNameFieldFinder = find.byValueKey('sign-up first name');
+    final lastNameFieldFinder = find.byValueKey('sign-up last name');
+    final usernameFieldFinder = find.byValueKey('sign-up username');
+    final emailFieldFinder = find.byValueKey('sign-up email');
+    final passwordFieldFinder = find.byValueKey('sign-up password');
+    final confirmPasswordFieldFinder = find.byValueKey('sign-up confirm password');
+    final noAccountWithEmailFinder = find.byValueKey('no account with this email');
+
 
     FlutterDriver driver;
 
@@ -29,6 +39,50 @@ void main() {
       if (driver != null) {
         driver.close();
       }
+    });
+
+    test('Create New Account', () async {
+      // tap sign-up
+      await driver.tap(signUpPageButtonFinder);
+      // fill out first name
+      await driver.tap(firstNameFieldFinder);
+      await driver.enterText('Yoni');
+      // fill out last name
+      await driver.tap(lastNameFieldFinder);
+      await driver.enterText('Gutenmacher');
+      // fill out username
+      await driver.tap(usernameFieldFinder);
+      await driver.enterText('yogutt');
+      // fill out email
+      await driver.tap(emailFieldFinder);
+      await driver.enterText('yonigg98@gmail.com');
+      // fill out password
+      await driver.tap(passwordFieldFinder);
+      await driver.enterText('Yoniguten1!');
+      // fill out confirm password
+      await driver.tap(confirmPasswordFieldFinder);
+      await driver.enterText('Yoniguten1!');
+      // tap sign up
+      await driver.tap(signUpButtonFinder);
+      // go into the app
+      // sign out and log-in with the same account
+      await driver.waitFor(homeFinder);
+      await driver.tap(userIconFinder);
+      await driver.waitFor(userPageFinder);
+      await driver.tap(signOutFinder);
+      await driver.waitFor(logInFinder);
+      await driver.tap(emailFinder);
+      await driver.enterText('yonigg98@gmail.com');
+      await driver.waitFor(find.text('yonigg98@gmail.com'));
+      await driver.tap(passwordFinder);
+      await driver.enterText('Yoniguten1!');
+      await driver.tap(buttonFinder);
+      await driver.waitFor(homeFinder);
+      // sign out again
+      await driver.tap(userIconFinder);
+      await driver.waitFor(userPageFinder);
+      await driver.tap(signOutFinder);
+      await driver.waitFor(logInFinder);
     });
 
     test('Log-in: valid credentials', () async {
@@ -54,27 +108,15 @@ void main() {
       await driver.enterText('Yoniguten1!');
       await driver.tap(buttonFinder);
       await driver.waitFor(homeFinder);
-    });
-
-    test('Create New Account', () async {
+      // sign out again
       await driver.tap(userIconFinder);
       await driver.waitFor(userPageFinder);
       await driver.tap(signOutFinder);
       await driver.waitFor(logInFinder);
-      await driver.tap(emailFinder);
-      await driver.enterText('yonigg98@gmail.com');
-      await driver.waitFor(find.text('yonigg98@gmail.com'));
-      await driver.tap(passwordFinder);
-      await driver.enterText('Yoniguten1!');
-      await driver.tap(buttonFinder);
-      await driver.waitFor(homeFinder);
     });
 
     test('Delete Account', () async {
-      await driver.tap(userIconFinder);
-      await driver.waitFor(userPageFinder);
-      await driver.tap(signOutFinder);
-      await driver.waitFor(logInFinder);
+      //sign-in
       await driver.tap(emailFinder);
       await driver.enterText('yonigg98@gmail.com');
       await driver.waitFor(find.text('yonigg98@gmail.com'));
@@ -82,20 +124,20 @@ void main() {
       await driver.enterText('Yoniguten1!');
       await driver.tap(buttonFinder);
       await driver.waitFor(homeFinder);
+      // delete the account
+      await driver.tap(userIconFinder);
+      await driver.waitFor(userPageFinder);
+      await driver.tap(deleteAccountFinder);
+      // try to log-in with same account
+      await driver.waitFor(logInFinder);
+      await driver.tap(emailFinder);
+      await driver.enterText('yonigg98@gmail.com');
+      await driver.waitFor(find.text('yonigg98@gmail.com'));
+      await driver.tap(passwordFinder);
+      await driver.enterText('Yoniguten1!');
+      await driver.tap(buttonFinder);
+      await driver.waitFor(noAccountWithEmailFinder);
     });
-
-
-    // tap sign-up
-    // fill out first name
-    // fill out last name
-    // fill out username
-    // fill out email
-    // fill out password
-    // fill out confirm password
-    // tap sign up
-    // go into the app
-    // sign out and log-in with the same account
-
 
     // delete the account and try to sign in again
 
